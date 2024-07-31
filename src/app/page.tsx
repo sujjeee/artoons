5b7cc4f9ae8902f957d3d31549917bab8e940dce
Suraj <sujjeee@outlook.com>
2024-07-31 20:43:55 +0530

    update: file structrue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import HeroShell from "@/components/hero-shell"
-import { ImagesShell } from "@/components/images-shell"
+import { Header } from "@/components/layouts/header"
 import React from "react"
 
 export default function Page() {
   return (
     <>
-      <div className="w-full pt-32  items-center justify-center flex">
-        <HeroShell />
-      </div>
-      <div className="pt-32">
-        <ImagesShell />
-      </div>
+      <Header />
     </>
   )
 }
---
+++ src/components/providers.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type { ThemeProviderProps } from "next-themes/dist/types"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <main>{children}</main>
+}

8c8b5609cc1f21064c9a8770394befa5093ae988
Suraj <sujjeee@outlook.com>
2024-09-04 11:10:11 +0530

    update: middleware

--- workers/src/middlewares.ts
+++ workers/src/middlewares.ts
@@ -15,45 +15,25 @@ export function rateLimitMiddleware({
   return async (c: Context<{ Bindings: Env }>, next: Next) => {
     const ip =
       c.req.header("x-vercel-forwarded-for") ||
-      c.req.header("CF-Connecting-IP") ||
+      c.req.header("cf-connecting-ip") ||
       "unknown-ip"
-
     const key = `${identifier}_${ip}`
+    const now = Date.now()
 
-    const data = (await c.env.RATE_LIMITER.get(key, { type: "json" })) as {
+    let data = (await c.env.RATE_LIMITER.get(key, { type: "json" })) as {
       count: number
       lastRequestTime: number
     } | null
 
-    const now = Date.now()
-
-    if (data) {
-      const { count, lastRequestTime } = data
-
-      // Reset the counter if the duration has passed
-      if (now - lastRequestTime > duration * 1000) {
-        await c.env.RATE_LIMITER.put(
-          key,
-          JSON.stringify({ count: 1, lastRequestTime: now }),
-        )
-      } else if (count < limit) {
-        // Increment the counter if within the rate limit
-        await c.env.RATE_LIMITER.put(
-          key,
-          JSON.stringify({ count: count + 1, lastRequestTime }),
-        )
-      } else {
-        // Rate limit exceeded
-        return c.text("Too many requests. Please try again later.", 429)
-      }
+    if (!data || now - data.lastRequestTime > duration * 1000) {
+      data = { count: 1, lastRequestTime: now }
+    } else if (data.count >= limit) {
+      return c.text("Too many requests. Please try again later.", 429)
     } else {
-      // Initialize counter for new IP
-      await c.env.RATE_LIMITER.put(
-        key,
-        JSON.stringify({ count: 1, lastRequestTime: now }),
-      )
+      data.count++
     }
 
+    await c.env.RATE_LIMITER.put(key, JSON.stringify(data))
     await next()
   }
 }
